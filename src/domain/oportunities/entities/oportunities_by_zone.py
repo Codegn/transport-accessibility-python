422513fa385990fa86fb13b3ddd9a49a39a950f6
Nicolás Villalobos Zaid <guillermo.nicolas@gmail.com>
2024-05-12 20:33:26 -0400

    refactor: improve typing in oportunities by zone class

--- src/domain/oportunities/entities/oportunities_by_zone.py
+++ src/domain/oportunities/entities/oportunities_by_zone.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from typing import Dict
+
 from src.domain.oportunities.entities.oportunity import Oportunity
 from src.domain.zone import Zone
 
+
 @dataclass
 class OportunitiesByZone:
-    data: dict[Zone, Oportunity]
+    data: Dict[Zone, Oportunity]
